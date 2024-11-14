db80c3f9055003271327c3a887831e6baa04eac7
Elenxta <164806114+Lenlenxo@users.noreply.github.com>
2024-11-14 09:32:48 +0000

    fix order

--- MyProject/Context/BookingProvider.cs
+++ MyProject/Context/BookingProvider.cs
@@ -1,10 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyProject.Components.Pages;
 using MyProject.Model;
 
 namespace MyProject.Context
 {
     public class BookingProvider
     {
+
+        private readonly DatabaseContext _context;
+
+        public BookingProvider(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        //public async Task CreateBooking(User user, IEnumerable<Service> service)
+        //{
+        //    // Create a new booking
+        //    var booking = new Booking
+        //    {
+        //        User = user,
+        //        Boooking = booking.Select(bookings => new Booking
+        //        {
+        //            Service = bookings.Service,       // Service being booked
+        //            Duration = bookings.Duration,     // Duration of the service
+        //            Stylist = bookings.Stylist
+        //        }).ToList(),
+        //        Created = DateTime.Now,
+
+        //    };
+
+        //    // Add the booking to the database
+        //    _context.Bookings.Add(booking);
+        //    await _context.SaveChangesAsync();
+
+        //}
     }
 }
     
