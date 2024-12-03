7689b91dc6a32196dae51b8504e770c12c2233ce
Elenxta <164806114+Lenlenxo@users.noreply.github.com>
2024-12-03 15:33:57 +0000

    new changes

--- MyProject/Model/Reviews.cs
+++ MyProject/Model/Reviews.cs
@@ -2,7 +2,7 @@
 {
     public class Review
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public int Stars { get; set; }
         public string Description { get; set; }
         public string Images { get; set; }
