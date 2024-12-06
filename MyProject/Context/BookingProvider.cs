8521edb81bfd27bb30192dc6cdffd53869512bee
Elenxta <164806114+Lenlenxo@users.noreply.github.com>
2024-12-06 10:02:07 +0000

    fix account page

--- MyProject/Context/BookingProvider.cs
+++ MyProject/Context/BookingProvider.cs
@@ -12,35 +12,32 @@ namespace MyProject.Context
             _context = context;
         }
 
-            public async Task CreateBooking(User user, int serviceId, DateTime date, TimeSpan time, int stylistId, string notes)
+      
+         public async Task CreateBooking(User user, int serviceId, DateTime date, TimeSpan time, int stylistId, string notes)
+        {
+            // Fetch the stylist from the database based on the provided stylistId
+            var stylist = await _context.Stylists.FirstOrDefaultAsync(s => s.Id == stylistId);
+
+            // Fetch the service from the database based on the provided serviceId
+            var service = await _context.Services.FirstOrDefaultAsync(s => s.Id == serviceId);
+
+            // Create a new booking object and set its properties
+            var booking = new Booking
             {
-                // Fetch the stylist from the database based on the provided stylistId
-                var stylist = await _context.Stylists.FirstOrDefaultAsync(s => s.Id == stylistId);
+                User = user,
+                Service = service,
+                Notes = notes,
+                Date = date,
+                Time = time,
+                Stylist = stylist,
+            };
 
-                // Fetch the service from the database based on the provided serviceId
-                var service = await _context.Services.FirstOrDefaultAsync(s => s.Id == serviceId);
+            // Add the new booking to the database context
+            _context.Bookings.Add(booking);
 
-                if (stylist == null) throw new InvalidOperationException("Stylist not found.");
-
-                if (service == null) throw new InvalidOperationException("Service not found.");
-
-                // Create a new booking object and set its properties
-                var booking = new Booking
-                {
-                    User = user,
-                    Service = service,
-                    Notes = notes,
-                    Date = date,
-                    Time = time,
-                    Stylist = stylist,
-                };
-
-                // Add the new booking to the database context
-                _context.Bookings.Add(booking);
-
-                // Save the changes to the database
-                await _context.SaveChangesAsync();
-            }
+            // Save the changes to the database
+            await _context.SaveChangesAsync();
         }
     }
+}
 
